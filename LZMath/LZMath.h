2ae75690b0424e09257eed069e108ab839f1c6ff
Luca Zapparoli <l.zapparoli@me.com>
2018-02-20 19:05:16 +0300

    Initial Commit

---
+++ LZMath/LZMath.h
@@ -0,0 +1,19 @@
+//
+//  LZMath.h
+//  LZMath
+//
+//  Created by Luca Zapparoli on 20/02/2018.
+//  Copyright © 2018 Luca Zapparoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LZMath.
+FOUNDATION_EXPORT double LZMathVersionNumber;
+
+//! Project version string for LZMath.
+FOUNDATION_EXPORT const unsigned char LZMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LZMath/PublicHeader.h>
+
+
